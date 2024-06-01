2db2174bcb53693d5e0e0a9caaeb725a19576c7f
vimansha <vimanshamadushani88@gmail.com>
2024-06-01 17:51:31 +0530

    update installed_apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
