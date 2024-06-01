40bb1ea14546f5061607142771338e6df3d1dc31
vimansha <vimanshamadushani88@gmail.com>
2024-06-02 00:00:59 +0530

    create a method that call if data is valid

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,3 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User    # model that we want to serialize as the User model
         fields = ["id", "username", "password"]     # fields that we want to serialize
+        extra_kwargs = { "password": { "write_only": True } }   # noone can read the pw
+
+    # implemet a method that call when want to create a new version of the user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
