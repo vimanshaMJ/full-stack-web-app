93e259df672769c61d25352c4834c5ea38cf17a1
vimansha <vimanshamadushani88@gmail.com>
2024-06-02 18:39:20 +0530

    make a serializer for this model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 # ORM - Object Relational Mapping
 # ORM: maps python objects to the corresponding code that need to be executed to make a change in the database
@@ -14,3 +15,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer( serializers.ModelSerializer ):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = { 'author': {'read_only' : True}}
+        
