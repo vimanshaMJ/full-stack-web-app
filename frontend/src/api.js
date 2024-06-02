ac9910ec09b7f1e85a2cccfaec8f3b2d33a16a61
vimansha <vimanshamadushani88@gmail.com>
2024-06-03 00:07:02 +0530

    pass JWT access token

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,3 +7,18 @@ import { ACCESS_TOKEN } from "./constants";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, //import anything that specified inside an environment file
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
