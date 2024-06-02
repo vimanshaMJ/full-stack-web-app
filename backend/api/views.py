29842ea6285e43067ec278c19858c2e5552204c9
vimansha <vimanshamadushani88@gmail.com>
2024-06-02 19:27:25 +0530

    make views for creating notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+## make view for creating a new note
+class NoteListCreate( generics.ListCreateAPIView ):     # ListCreateAPIView: list all notes or create new note
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  # cannot call this rout unless you're authenticated and you pass a valid JWT token
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author = user)
+# in django, if we want to get the user that is actually authenticated and that is interacting with this rout, write self.request.user
+
+    def perform_create(self, serializer):   #overriding the create method
+        if serializer.is_valid():
+            serializer.save(author = self.request.user)
+        else:
+            print(serializer.errors)
+        
+
 
 # Create your views here.
 class CreateUserView( generics.CreateAPIView ):     # this generics view will atomatically create a new object for us 
