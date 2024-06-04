081c24d8c416b29daf10869af1798dff5b1c73f0
vimansha <vimanshamadushani88@gmail.com>
2024-06-04 23:01:22 +0530

    Logout and register functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+// loging us out
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+// if somebody is registering, first clear the local storage. So we dont have any access token
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
 
 function App() {
-  return <></>;
+  return;
+  <></>;
 }
 
 export default App;
