ec5e8f84966fc7d37e9df56daf53599d180b4a9c
vimansha <vimanshamadushani88@gmail.com>
2024-06-05 22:53:54 +0530

    form name login/register

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,3 +2,23 @@ import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+// for logging & registering
+export default function Form({ route, method }) {
+  const [username, setUsername] = useState("");
+  const [password, setpassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const name = method === "login" ? "Login" : "Register";
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>{name}</h1>
+    </form>
+  );
+}
