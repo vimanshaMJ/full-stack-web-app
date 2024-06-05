12d699d5e6bb7f4e51531a1a2e7a1007be818132
vimansha <vimanshamadushani88@gmail.com>
2024-06-06 01:14:34 +0530

    small change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Form from "../components/Form";
 
 export default function Login() {
-  return <Form route="/api/token/login/" method="login" />;
+  return <Form route="/api/token/" method="login" />;
 }
