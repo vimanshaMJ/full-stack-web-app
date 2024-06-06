2125fc97bbfb6a2078178a4a1984b0ca57afa673
vimansha <vimanshamadushani88@gmail.com>
2024-06-06 12:54:55 +0530

    home content html

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -34,9 +34,9 @@ export default function Home() {
       .then((res) => {
         if (res.status === 204) alert("Note Deleted!");
         else alert("Failed to delete note.");
+        getNote(); // update the screen by removing the notes when recieve the new notes from backend
       })
       .catch((err) => alert(err));
-    getNote(); // update the screen by removing the notes when recieve the new notes from backend
   };
 
   //create notes
@@ -47,10 +47,40 @@ export default function Home() {
       .then((res) => {
         if (res.status === 201) alert("Note Created!");
         else alert("Failed to create.");
+        getNote();
       })
       .catch((err) => alert(err));
-    getNote();
   };
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <h1>Notes</h1>
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
