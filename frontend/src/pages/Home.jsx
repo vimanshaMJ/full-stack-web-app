0b107ae98ba01ac194a9ce0f99090389ee00ea6a
vimansha <vimanshamadushani88@gmail.com>
2024-06-06 11:24:57 +0530

    states we need in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import api from "../api";
 
 export default function Home() {
+  //need to keep track of all notes we already grabbed from the server
+  const [notes, setNotes] = useState([]);
+
+  //state allow us to create new notes
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
   return <div>Home</div>;
 }
